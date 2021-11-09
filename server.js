5ae3090275af8b624bb9f39c96e62e77f41b2d34
clarani <clara.ni@student-cs.fr>
2021-11-09 11:28:23 +0100

    Connexion à MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { MONGO_URI } = require('./config');
+
 const app = express();
+
+mongoose.connect(MONGO_URI, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+})
+   .then(() => console.log('MongoDB connected!'))
+   .catch(err => console.log(err));
+
+
 app.get('/', (req, res) => {
-   res.send('Hello World');
+   res.send('Hello World, database is connected');
 });
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
