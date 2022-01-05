35908cba95de63147a520692194762b1f163b4f2
clarani <clara.ni@student-cs.fr>
2022-01-05 15:48:29 +0100

    Correction du backApi pour récupérer seulement les derniers status

--- src/routes/station_route.js
+++ src/routes/station_route.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router  = express.Router(); 
 const StationStatus = require('./../models/station-status-model') // new
-const StationInformation = require('./../models/station-information-model') // new
+const StationInformation = require('./../models/station-information-model'); // new
+const { status } = require('express/lib/response');
 
 // const stationController = require('../controllers/station_controller');
 
 router.get('/station_status', async (req, res) => {
-	const status = await StationStatus.find().sort({ 'last_updated': -1 })
+	const lastStatus = await StationStatus.findOne().sort({ 'last_updated': -1 }); // récupérer la dernière date d'update
+	const status = await StationStatus.find({ 'last_updated': lastStatus.last_updated }) // récupérer les status à cette date
 	res.send(status)
 })
 
