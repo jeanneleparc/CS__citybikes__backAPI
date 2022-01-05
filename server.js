0134c88159abaef52c4f3565acc9ed4428f12d34
clarani <clara.ni@student-cs.fr>
2022-01-05 15:48:29 +0100

    Adaptation du backAPI aux nouveaux schémas de la bdd (crawler&browker)

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ const app = express();
 
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/citibikes';
 
+// connect to database
 mongoose.connect(MONGO_URI, {
    useNewUrlParser: true,
    useUnifiedTopology: true,
 })
    .then(() => {
       app.use(express.json());
-		app.use("/", routes) // new
+		app.use("/", routes)
       console.log('MongoDB connected!')
    })
    .catch(err => console.log(err));
--- src/routes/station_route.js
+++ src/routes/station_route.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router  = express.Router(); 
-const StationStatus = require("./../models/station-status-model") // new
-const StationInformation = require("./../models/station-information-model") // new
+const StationStatus = require('./../models/station-status-model') // new
+const StationInformation = require('./../models/station-information-model') // new
 
-// const stationController = require('../controllers/station_controller'); 
+// const stationController = require('../controllers/station_controller');
 
-router.get("/station_status", async (req, res) => {
-	const status = await StationStatus.find().sort({ "last_updated": -1 }).limit(1)
+router.get('/station_status', async (req, res) => {
+	const status = await StationStatus.find().sort({ 'last_updated': -1 })
 	res.send(status)
 })
 
-router.get("/station_information", async (req, res) => {
-	const information = await StationInformation.find().sort({ "last_updated": -1 }).limit(1)
+router.get('/station_information', async (req, res) => {
+	const information = await StationInformation.find().sort({ 'last_updated': -1 })
 	res.send(information)
 })
 module.exports = router; 
