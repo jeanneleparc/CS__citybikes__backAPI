3489aa4aaaff6cd39ac308b3be4d91c16665d5a0
clarani <clara.ni@student-cs.fr>
2022-01-06 10:15:36 +0100

    Adaptation du backAPI aux nouveaux schémas de données (changement causé par le crawler et le browker) (#4)
    * Adaptation du backAPI aux nouveaux schémas de la bdd (crawler&browker)
    
    * Correction du backApi pour récupérer seulement les derniers status
    
    * tranduction commentaire
    
    Co-authored-by: Bouyssel Camille <camille.bouyssel@student-cs.fr>

--- src/routes/station_route.js
+++ src/routes/station_route.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const router  = express.Router(); 
-const StationStatus = require("./../models/station-status-model") // new
-const StationInformation = require("./../models/station-information-model") // new
+const StationStatus = require('./../models/station-status-model') // new
+const StationInformation = require('./../models/station-information-model'); // new
+const { status } = require('express/lib/response');
 
-// const stationController = require('../controllers/station_controller'); 
+// const stationController = require('../controllers/station_controller');
 
-router.get("/station_status", async (req, res) => {
-	const status = await StationStatus.find().sort({ "last_updated": -1 }).limit(1)
+router.get('/station_status', async (req, res) => {
+	const lastStatus = await StationStatus.findOne().sort({ 'last_updated': -1 }); // get the last update date
+	const status = await StationStatus.find({ 'last_updated': lastStatus.last_updated }) // get all status for this date
 	res.send(status)
 })
 
-router.get("/station_information", async (req, res) => {
-	const information = await StationInformation.find().sort({ "last_updated": -1 }).limit(1)
+router.get('/station_information', async (req, res) => {
+	const information = await StationInformation.find().sort({ 'last_updated': -1 })
 	res.send(information)
 })
 module.exports = router; 
