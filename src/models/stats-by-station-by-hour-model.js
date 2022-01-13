d73befc8e42642114b597df35f4a18d4b2cb9c76
jeanneleparc <jeanne.leparc@wanadoo.fr>
2022-01-13 10:45:00 +0100

    ajout model stats

---
+++ src/models/stats-by-station-by-hour-model.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var statsByStationByHourSchema = mongoose.Schema({
+    station_id: Number,
+    station_name: String,
+    station_long: Number,
+    station_lat: Number,
+    time_slot: Number, 
+    filling_rate: Number,
+    avg_bikes_nb: Number,
+    date: Date,
+});
+
+var statsByStationByHour = mongoose.model('statsbystationbyhour', statsByStationByHourSchema);
+
+module.exports = statsByStationByHour;
+
+module.exports.get = function (callback, limit) {
+    statsByStationByHour.find(callback).limit(limit);
+}
