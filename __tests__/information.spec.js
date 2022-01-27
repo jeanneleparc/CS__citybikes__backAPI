5f987dc255e8f611cfd212fb619f5d9fe656b336
BOUYSSEL Camille <45000497+BouysselCamille@users.noreply.github.com>
2022-01-27 10:43:51 +0100

    Test (#9)
    * test api routes with jest
    
    * test on ci
    
    * ajout test information without data

---
+++ __tests__/information.spec.js
@@ -0,0 +1,62 @@
+// Appel des modules
+const request = require("supertest");
+const mongoose = require("mongoose");
+const app = require("../app");
+const Information = require("../src/models/station-information-model");
+
+const url = "/station_information";
+
+/* Supprimer tous les status */
+function deleteAll() {
+  // Requête Mongoose - https://mongoosejs.com/docs/api.html#query_Query-deleteMany
+  return Information.deleteMany({})
+    .then((result) => result)
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+beforeAll(() => deleteAll());
+
+afterAll(() => {
+  mongoose.connection.close();
+});
+
+describe("Route Information", () => {
+  const mockInformation = {
+    id: 1,
+    name: "station",
+    longitude: 0,
+    latitude: 0,
+    capacity: 2,
+    last_updated: new Date(),
+    has_kiosk: true,
+  };
+
+  test("#1 - GET / - Without data", async () => {
+    const response = await request(app).get(url);
+    expect(response).toBeDefined();
+    expect(response.statusCode).toBe(200);
+    expect(response.body.length).toBe(0);
+  });
+
+  test("#2 - GET / - Good", async () => {
+    // Populate DB
+    await Information.create(mockInformation);
+
+    const response = await request(app).get(`${url}`);
+
+    expect(response).toBeDefined();
+    expect(response.statusCode).toBe(200);
+    expect(response.body.length).toBe(1);
+    expect(response.body[0].id).toBe(mockInformation.id);
+    expect(response.body[0].name).toBe(mockInformation.name);
+    expect(response.body[0].longitude).toBe(mockInformation.longitude);
+    expect(response.body[0].latitude).toBe(mockInformation.latitude);
+    expect(response.body[0].capacity).toBe(mockInformation.capacity);
+    expect(response.body[0].last_updated).toBe(
+      mockInformation.last_updated.toISOString()
+    );
+    expect(response.body[0].has_kiosk).toBe(mockInformation.has_kiosk);
+  });
+});
---
+++ __tests__/status.spec.js
@@ -0,0 +1,83 @@
+// Appel des modules
+const request = require("supertest");
+const mongoose = require("mongoose");
+const app = require("../app");
+const Status = require("../src/models/station-status-model");
+
+const url = "/station_status";
+
+/* Supprimer tous les status */
+function deleteAll() {
+  // Requête Mongoose - https://mongoosejs.com/docs/api.html#query_Query-deleteMany
+  return Status.deleteMany({})
+    .then((result) => result)
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+beforeAll(() => deleteAll());
+
+afterAll(() => {
+  mongoose.connection.close();
+});
+
+describe("Route Status", () => {
+  const mockStatus = {
+    id: 1,
+    name: "station",
+    longitude: 0,
+    latitude: 0,
+    capacity: 2,
+    num_docks_available: 1,
+    num_bikes_available: 1,
+    num_docks_disabled: 1,
+    num_bikes_disabled: 1,
+    num_ebikes: 1,
+    station_status: "active",
+    is_installed: true,
+    last_updated: new Date(),
+    has_kiosk: true,
+  };
+  test("#1 - GET / - Without data return 202", async () => {
+    const response = await request(app).get(url);
+    expect(response).toBeDefined();
+    expect(response.statusCode).toBe(202);
+    expect(response.body.message).toBe("No status in collection");
+  });
+
+  test("#2 - GET / - Good", async () => {
+    // Populate DB
+    await Status.create(mockStatus);
+
+    const response = await request(app).get(`${url}`);
+
+    expect(response).toBeDefined();
+    expect(response.statusCode).toBe(200);
+    expect(response.body.length).toBe(1);
+    expect(response.body[0].id).toBe(mockStatus.id);
+    expect(response.body[0].name).toBe(mockStatus.name);
+    expect(response.body[0].longitude).toBe(mockStatus.longitude);
+    expect(response.body[0].latitude).toBe(mockStatus.latitude);
+    expect(response.body[0].capacity).toBe(mockStatus.capacity);
+    expect(response.body[0].num_docks_available).toBe(
+      mockStatus.num_docks_available
+    );
+    expect(response.body[0].num_bikes_available).toBe(
+      mockStatus.num_bikes_available
+    );
+    expect(response.body[0].num_docks_disabled).toBe(
+      mockStatus.num_docks_disabled
+    );
+    expect(response.body[0].num_bikes_disabled).toBe(
+      mockStatus.num_bikes_disabled
+    );
+    expect(response.body[0].num_ebikes).toBe(mockStatus.num_ebikes);
+    expect(response.body[0].station_status).toBe(mockStatus.station_status);
+    expect(response.body[0].is_installed).toBe(mockStatus.is_installed);
+    expect(response.body[0].last_updated).toBe(
+      mockStatus.last_updated.toISOString()
+    );
+    expect(response.body[0].has_kiosk).toBe(mockStatus.has_kiosk);
+  });
+});
